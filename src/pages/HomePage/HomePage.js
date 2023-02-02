f0c660ea2ed43e68c77550266afb0902be76b1cb
Jorfishdan <jordan_fish@ymail.com>
2023-02-02 13:05:59 -0800

    planning out layout

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,16 @@
 import "./HomePage.scss"
 import logo from "../../assets/images/teachers-pet.png";
 import spinCat from "../../assets/images/spinning-cat.gif";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 function HomePage() {
     return (
         <>
+        <Header />
         <div className="homepage"> This is a test
         <img src={logo} alt="logo of cat" />
-        <img src={spinCat} alt="spinning cat gif" />
         </div>
+        <Footer />
         </>
     )
 }
