a24fc547ab4e0f919361fa9352df5d0bba163828
Jorfishdan <jordan_fish@ymail.com>
2023-02-06 13:06:16 -0800

    added stopwatch

--- src/pages/CleaningPage/CleaningPage.js
+++ src/pages/CleaningPage/CleaningPage.js
@@ -1,7 +1,24 @@
+import { useStopwatch } from "react-timer-hook";
 import "./CleaningPage.scss"
 
+
 function CleaningPage() {
-    return
+    const {seconds, minutes, isRunning, start, pause,reset,} = useStopwatch({autoStart:true})
+    return (
+        <>
+         <div style={{textAlign: 'center'}}>
+      <h1>react-timer-hook</h1>
+      <p>Stopwatch Demo</p>
+      <div style={{fontSize: '100px'}}>
+    <span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+        </>
+    )
 }
 
 export default CleaningPage;
