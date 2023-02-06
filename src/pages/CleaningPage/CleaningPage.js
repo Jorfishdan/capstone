2ed6dc9f54813b50767de5e3a5600e92bebe3471
Jorfishdan <jordan_fish@ymail.com>
2023-02-06 13:13:28 -0800

    changed it to a timer countdown

--- src/pages/CleaningPage/CleaningPage.js
+++ src/pages/CleaningPage/CleaningPage.js
@@ -1,24 +1,33 @@
-import { useStopwatch } from "react-timer-hook";
-import "./CleaningPage.scss"
+import { useStopwatch, useTimer } from "react-timer-hook";
+import "./CleaningPage.scss";
 
+function CleaningPage({expiryTimestamp}) {
+  const { seconds, minutes, isRunning, start, pause, reset, restart } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
 
-function CleaningPage() {
-    const {seconds, minutes, isRunning, start, pause,reset,} = useStopwatch({autoStart:true})
-    return (
-        <>
-         <div style={{textAlign: 'center'}}>
-      <h1>react-timer-hook</h1>
-      <p>Stopwatch Demo</p>
-      <div style={{fontSize: '100px'}}>
-    <span>{minutes}</span>:<span>{seconds}</span>
+  return (
+    <>
+      <div style={{ textAlign: "center" }}>
+        <h1>Lightning Speed Clean</h1>
+        <p>Can you beat the clock?</p>
+        <div style={{ fontSize: "100px" }}>
+          <span>{minutes}</span>:<span>{seconds}</span>
+        </div>
+        <p>{isRunning ? "Running" : "Not running"}</p>
+        <button onClick={start}>Start</button>
+        <button onClick={pause}>Pause</button>
+        <button onClick={reset}>Reset</button>
+        <button
+          onClick={() => {
+            const time = new Date();
+            time.setSeconds(time.getSeconds() + 300);
+            restart(time);
+          }}
+        >
+          Restart
+        </button>
       </div>
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
-      <button onClick={start}>Start</button>
-      <button onClick={pause}>Pause</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-        </>
-    )
+    </>
+  );
 }
 
 export default CleaningPage;
