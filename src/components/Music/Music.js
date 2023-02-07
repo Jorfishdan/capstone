8169273380a23c34496930b3d7b519e502d40d5b
Jorfishdan <jordan_fish@ymail.com>
2023-02-06 16:10:26 -0800

    added keyframe to cat to spin every 2 min

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -39,6 +39,7 @@ function Music() {
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/297008220&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
       ></iframe>
       <iframe
+        className="cleanup__music"
         width="40%"
         height="166"
         scrolling="no"
@@ -46,7 +47,15 @@ function Music() {
         allow="autoplay"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/135813675&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
       ></iframe>
-      <iframe width="40%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/262844816&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+      <iframe
+      className="cleanup__music"
+        width="40%"
+        height="166"
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/262844816&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      ></iframe>
     </>
   );
 }
--- src/pages/CleaningPage/CleaningPage.js
+++ src/pages/CleaningPage/CleaningPage.js
@@ -2,6 +2,7 @@ import "./CleaningPage.scss";
 import Timer from "../../components/Timer/Timer";
 import Music from "../../components/Music/Music";
 import { useState } from "react";
+import pet from "../../assets/images/smiling-cat.png";
 
 function CleaningPage() {
     // const [openModal, setOpenModal] = useState(false);
@@ -10,6 +11,8 @@ return (
     <>
     <Timer setPage={setPage}/>
     <Music />
+    <img src={pet} alt="cat cartoon" className="cleaningpage__cat"/>
+
     </>
 )
 }
