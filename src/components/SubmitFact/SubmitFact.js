46ff6c85f9a62750fb9bedfeac7110ab02605854
Jorfishdan <jordan_fish@ymail.com>
2023-02-07 20:06:33 -0800

    form works minus it not clearing fields after submission

--- src/components/SubmitFact/SubmitFact.js
+++ src/components/SubmitFact/SubmitFact.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import {v4 as uuidv4} from "uuid";
 import './SubmitFact.scss';
 
 
 function SumbitFact() { 
+
     const formRef = useRef();
+    const [clearSubQuestion, setClearSubQuestion] = useState(null)
 
     function handleSubmit(event) {
         event.preventDefault()
@@ -15,7 +17,24 @@ function SumbitFact() {
         const incorrect = form.incorrect.value;
         const explain = form.explain.value;
 
-    }
+        if ( !question || !answer || !incorrect || !explain) {
+            alert("Please fill out all the fields")
+            return;
+        }
+
+    axios.post(`http://localhost:8080/facts`, {
+        category: "Misc",
+        type: Boolean,
+        difficulty: "easy",
+        question: question,
+        correct_answer: answer,
+        incorrect_answer: incorrect,
+        explanation: explain,
+    })
+    alert("It's been added to the game!")
+
+};
+
     return (
         <form onSumbit={handleSubmit} ref={formRef} className="form__submit">
             <h2 className="form__add-question">Add a question to the game!</h2>
@@ -29,6 +48,8 @@ function SumbitFact() {
 
             <h2 className="form__add-explain">Add an explanantion</h2>
             <input className="form__explain" type="text" name="explain" placeholder="Enter here"></input>
+
+            <button className="form__button" onClick={handleSubmit} >Submit</button>
 
         </form>
     )
