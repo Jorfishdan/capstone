a3ce74b4415668f9908b43a2bce6c61a72fcb6f1
Jorfishdan <jordan_fish@ymail.com>
2023-02-09 15:35:21 -0800

    updated button styling for homepage

--- src/components/HomePageCards/HomePageCards.js
+++ src/components/HomePageCards/HomePageCards.js
@@ -10,9 +10,9 @@ function HomePageCards() {
       <div className="homepagecard__mega-wrapper">
       {/* <img src={logo} alt="logo" className="homepagecard__logo" /> */}
     <article className="homepagecard__wrapper">
-     <Link to="/brainTeaser"><h1 className="homepagecard__card--1">Brain Teaser</h1></Link> 
-      <Link to="/cleanup"><h1 className="homepagecard__card--2">Clean Up</h1></Link>
-      <Link to="/callbacks"><h1 className="homepagecard__card--3">Call Backs</h1></Link>
+     <Link to="/brainTeaser"><h1 className="homepagecard__card--1">Head Scatchers</h1></Link> 
+      <Link to="/cleanup"><h1 className="homepagecard__card--2">Lightning Clean</h1></Link>
+      <Link to="/callbacks"><h1 className="homepagecard__card--3">Sneaky Animals</h1></Link>
     </article>
     </div>
     </section>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,16 +4,20 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import HomePageCards from "../../components/HomePageCards/HomePageCards";
 import dog from "../../assets/images/dogMascot.gif";
+import dog2 from "../../assets/images/dog2.png";
 
 function HomePage() {
     return (
         <>
         <section className="homepage">
-        <Header />
-        <HomePageCards />
-        <Footer />
-        </section>        
+        {/* <Header /> */}
+        <div className="homepage__circle"></div>
         <img className="homepage__dog" src={dog} alt="dog" />
+        <HomePageCards />
+        {/* <Footer /> */}
+        
+        </section>        
+        
         </>
     )
 }
