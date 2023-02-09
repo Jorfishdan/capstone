02bf6d17cdf7eaffe0e3aaab069caceaefe33a92
Jorfishdan <jordan_fish@ymail.com>
2023-02-08 18:46:04 -0800

    new timer works to input minutes but still need to fix stop and start button

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,44 +1,67 @@
 import "./Timer.scss"
-import { useTimer } from "react-timer-hook";
+import {  CountdownCircleTimer } from "react-countdown-circle-timer";
 import CleaningModal from "../CleaningModal/CleaningModal";
 import { useState } from "react";
 
-function Timer ({  setPage, setInputTime }) {
-    
+function Timer ({  setPage, inputTime }) {
+
+  
     const [openModal, setOpenModal] = useState(false);
     const [input, setInput] = useState(0);
-    const expiryTimestamp = setInputTime();
+    const renderTime = ({ remainingTime }) => {
+      if (remainingTime <= 0) {
+        return <div className="timer">Too late...</div>;
+       
+      }
+      const remainingMinutes = Math.ceil(remainingTime / 60);
+      
+      return (
+        <>
 
-    const { seconds, minutes, isRunning, start, pause, restart } =
-    useTimer({
-      expiryTimestamp,
-      onExpire: () => console.warn("Time's up!"),
-    });
-
-    function startScan() {
-        setOpenModal(true);
-    }
-
-    function handleChange(event) {
-      event.preventDefault();
-      setInput(event.target.value * 60)
-    }
-
-  return (
-    <>
-      <div style={{ textAlign: "center" }}>
-        <h1 className="timer__title">Lightning Speed Clean</h1>
-        <p className="timer__cta">Can you beat the clock?</p>
-        <div style={{ fontSize: "100px" }}>
-          <span className="timer__minutes">{minutes}</span>:<span className="timer__seconds">{seconds}</span>
+        <div className="timer">
+          <div className="text">Remaining</div>
+          <div className="value">{remainingMinutes}</div>
+          <div className="text">minutes</div>
         </div>
-        <p className="timer__running">{isRunning ? "Running" : "Not running"}</p>
+        <CleaningModal openModal={openModal} onClose={() => setOpenModal(false)} setPage={setPage} />
+        </>
+      );
+
+    }
+    function handleChange(event) {
+            event.preventDefault();
+            setInput(event.target.value * 60)
+          }
+        // function startScan() {
+        //             setOpenModal(true);
+        //         }
+
+              const { seconds, minutes, isRunning, start, pause, restart } =
+              CountdownCircleTimer
+
+      return (
+        <div className="timer">
+        
+        <div className="timer-wrapper">
+          <CountdownCircleTimer
+            isPlaying
+            duration={input}
+            colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+            colorsTime={[10, 6, 3, 0]}
+            onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+          >
+            {renderTime}
+          </CountdownCircleTimer>
+        </div>
+        
         <form className="timer__form">
-            <input type="number" step="60" onChange={handleChange}/>
-        </form>
-        <button className="timer__start" onClick={start}>Start</button>
+            <input type="number" step="1" onChange={handleChange}/>
+            <span>minutes</span>
+       </form>
+
+       <button className="timer__start" onClick={start}>Start</button>
         <button className="timer__pause"onClick={pause}>Pause</button>
-        <button className="timer__countdown"
+         {/* <button className="timer__countdown"
           onClick={() => {
             const time = new Date();
             time.setMinutes(time.getMinutes()+ parseInt(input)/60);
@@ -46,15 +69,93 @@ function Timer ({  setPage, setInputTime }) {
           }}
         >
           Restart
-        </button>
-        <button onClick={() => startScan()} className="timer__scan">Scan</button>
-        </div>
+        </button> */}
+        {/* <button onClick={() => startScan()} className="timer__scan">Scan</button> */}
         <CleaningModal openModal={openModal} onClose={() => setOpenModal(false)} setPage={setPage} />
-    </>
-  );
-}
+      </div>
+      )
+    };
+    
+   
+  export default Timer;
 
-export default Timer;
+  // return (
+  //   <>
+  //     <div style={{ textAlign: "center" }}>
+  //       <h1 className="timer__title">Lightning Speed Clean</h1>
+  //       <p className="timer__cta">Can you beat the clock?</p>
+  //       <div className="timer">
+  //         <div className="text">Remaining</div>
+  //         <div className="value">{remainingTime}</div>
+  //         <div className="text">seconds</div>
+  //       </div>
+  //       </div>
+  //       <CleaningModal openModal={openModal} onClose={() => setOpenModal(false)} setPage={setPage} />
+  //   </>
+  // );
+// }
+
+// export default Timer;
+
+
+////////////////////ORIGINAL TIMER -IT WORKS!//////////////////////////////////////
+// import "./Timer.scss"
+// import { useTimer } from "react-timer-hook";
+// import CleaningModal from "../CleaningModal/CleaningModal";
+// import { useState } from "react";
+
+// function Timer ({  setPage, setInputTime }) {
+    
+//     const [openModal, setOpenModal] = useState(false);
+//     const [input, setInput] = useState(0);
+//     const expiryTimestamp = setInputTime();
+
+//     const { seconds, minutes, isRunning, start, pause, restart } =
+//     useTimer({
+//       expiryTimestamp,
+//       onExpire: () => console.warn("Time's up!"),
+//     });
+
+//     function startScan() {
+//         setOpenModal(true);
+//     }
+
+//     function handleChange(event) {
+//       event.preventDefault();
+//       setInput(event.target.value * 60)
+//     }
+
+//   return (
+//     <>
+//       <div style={{ textAlign: "center" }}>
+//         <h1 className="timer__title">Lightning Speed Clean</h1>
+//         <p className="timer__cta">Can you beat the clock?</p>
+//         <div style={{ fontSize: "100px" }}>
+//           <span className="timer__minutes">{minutes}</span>:<span className="timer__seconds">{seconds}</span>
+//         </div>
+//         <p className="timer__running">{isRunning ? "Running" : "Not running"}</p>
+//         <form className="timer__form">
+//             <input type="number" step="60" onChange={handleChange}/>
+//         </form>
+//         <button className="timer__start" onClick={start}>Start</button>
+//         <button className="timer__pause"onClick={pause}>Pause</button>
+//         <button className="timer__countdown"
+//           onClick={() => {
+//             const time = new Date();
+//             time.setMinutes(time.getMinutes()+ parseInt(input)/60);
+//             restart(time.getTime());
+//           }}
+//         >
+//           Restart
+//         </button>
+//         <button onClick={() => startScan()} className="timer__scan">Scan</button>
+//         </div>
+//         <CleaningModal openModal={openModal} onClose={() => setOpenModal(false)} setPage={setPage} />
+//     </>
+//   );
+// }
+
+// export default Timer;
 
 
 //////////////////////BACKUP VERSION--> THIS WORKS BUT CAN'T SET THE MINUTES/////////////////////////////////
--- src/pages/CleaningPage/CleaningPage.js
+++ src/pages/CleaningPage/CleaningPage.js
@@ -16,7 +16,7 @@ function CleaningPage() {
 return (
     <>
     <Link to="/"><img src={home} alt="home icon to press to return to the home page" className="cleaningpage__home" /></Link>
-    <Timer setPage={setPage}  setInputTime={setInputTime}/>
+    <Timer setPage={setPage} expiryTimestamp={time} setInputTime={setInputTime}/>
     <Music />
     <img src={pet} alt="cat cartoon" className="cleaningpage__cat"/>
     </>
@@ -25,4 +25,30 @@ return (
 
 export default CleaningPage;
 
-// expiryTimestamp={time}
+/////////////////Orginial clock set up//////////////
+// import "./CleaningPage.scss";
+// import Timer from "../../components/Timer/Timer";
+// import Music from "../../components/Music/Music";
+// import { useState } from "react";
+// import pet from "../../assets/images/smiling-cat.png";
+// import home from "../../assets/images/home.png";
+// import { Link } from "react-router-dom";
+
+// function CleaningPage() {
+//     const [page, setPage] = useState(null)
+//     const [inputTime, setInputTime] = useState(0)
+
+//     const time = new Date ();
+
+// return (
+//     <>
+//     <Link to="/"><img src={home} alt="home icon to press to return to the home page" className="cleaningpage__home" /></Link>
+//     <Timer setPage={setPage} expiryTimestamp={time} setInputTime={setInputTime}/>
+//     <Music />
+//     <img src={pet} alt="cat cartoon" className="cleaningpage__cat"/>
+//     </>
+// )
+// }
+
+// export default CleaningPage;
+
