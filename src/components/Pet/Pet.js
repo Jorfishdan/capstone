237a858e964d5903807b3ed630ee2ec7498667ef
Jorfishdan <jordan_fish@ymail.com>
2023-02-10 14:42:13 -0800

    added axios to speech bubble

--- src/components/Pet/Pet.js
+++ src/components/Pet/Pet.js
@@ -2,8 +2,10 @@ import "./Pet.scss"
 import cheering from "../../assets/images/cheering.gif";
 import chat from "../../assets/images/chat.png";
 import { useEffect, useState } from "react";
+import axios from "axios";
 function Pet() {
     const [isPetShown, setIsPetShown] = useState(false);
+    const [dogBubble, setDogBubble] = useState([])
 
     useEffect(() => {
         setTimeout(() => {
@@ -19,6 +21,23 @@ function Pet() {
     }
     }, [isPetShown])
 
+    useEffect(() => {
+        const dogSays = async () => {
+          try {
+            const response = await axios.get(
+              ` http://localhost:8080/dog`
+            );
+            console.log(response.data[0])
+            // setDogBubble(response.data);
+            setDogBubble(response.data[0])
+        
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        dogSays();
+      }, []);
+
     return (
         <div className="pet">
             {isPetShown && ( 
@@ -26,7 +45,7 @@ function Pet() {
             <img src={cheering} alt="speech bubble" className="pet__cheering" />
             <div className="pet__bubble-wrap">
             <img src={chat} alt="speech bubble" className="pet__chat" />
-            <span className="pet__bubble-text">Go class! I know you can beat the clock -woof!</span>
+            <span className="pet__bubble-text">{dogBubble.say}</span>
             </div>
             
         </div>
