f6255ef84063137f31c3a657a6914de176df3053
Jorfishdan <jordan_fish@ymail.com>
2023-02-09 17:49:25 -0800

    added info button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import info from "../../assets/images/info.png";
 import list from "../../assets/images/list.png";
 import logout from "../../assets/images/logout.png";
 import information from "../../assets/images/information.gif"
+import staticInfo from "../../assets/images/info-static.png";
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -12,7 +13,7 @@ function Header() {
             <div className="header__wrapper">
                 {/* <img src={logout} alt="logout icon" className="header__logout" /> */}
                 {/* <img src={list} alt="list icon to edit profile" className="header__list"></img> */}
-               {/* <Link to="/mainInfo"><div className="header__info"></div></Link>                 */}
+               <Link to="/mainInfo"><img src={staticInfo}className="header__info"></img></Link>                
             </div>
         </section>
     )
