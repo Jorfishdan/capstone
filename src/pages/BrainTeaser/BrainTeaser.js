3e7c9119fd7098cb184ecf318dc319e678d3874c
Jorfishdan <jordan_fish@ymail.com>
2023-02-09 21:17:08 -0800

    saving before bed

--- src/pages/BrainTeaser/BrainTeaser.js
+++ src/pages/BrainTeaser/BrainTeaser.js
@@ -3,17 +3,29 @@ import BrainTeaserCard from "../../components/BrainTeaserCard/BrainTeaserCard";
 import home from "../../assets/images/home.png";
 import house from "../../assets/images/house-static.png";
 import SubmitFact from "../../components/SubmitFact/SubmitFact";
+import dogfood from "../../assets/images/dog-food.png";
+import paw from "../../assets/images/pawprint.png";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 function BrainTeaser() {
     
     return(
         <>
         <section className="brainteaser">
+            <div className="brainteaser__header">
         <Link to="/">
             <img src={house} alt="home icon to return to main menu" className="brainteaser__home" />
             </Link>
+            <div className="brainteaser__icon-wrapper">
+            <Link to="/cleanup">
+            <img src={dogfood} alt="home icon to return to main menu" className="brainteaser__home" />
+            </Link>
+            <Link to="/callbacks">
+            <img src={paw} alt="home icon to return to main menu" className="brainteaser__home" />
+            </Link>
+            </div>
+            </div>
             <div className="brainteaser__cardA"></div>
             <div className="brainteaser__cardB"></div>
             <div className="brainteaser__cardC"></div>
