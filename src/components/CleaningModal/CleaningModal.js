c5dda4ce8440bffce21cb844cc3e85c7554c6a59
Jorfishdan <jordan_fish@ymail.com>
2023-02-11 15:48:21 -0800

    added countdown to callbacks

--- src/components/CleaningModal/CleaningModal.js
+++ src/components/CleaningModal/CleaningModal.js
@@ -1,17 +1,38 @@
 import "./CleaningModal.scss";
 import closeBtn from "../../assets/images/close.png";
 import sniffing from "../../assets/images/sniffing.gif";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import chat from "../../assets/images/chat.png";
+import axios from "axios";
 
 function CleaningModal({ openModal, onClose}) {
+
   const [isShown, setIsShown] = useState(false);
+  // const [scanSays, setScanSays] = useState({});
   if (!openModal) return null;
+
+  // useEffect(() => {
+  //   if (!openModal) return;
+  //   const scanResults = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         ` http://localhost:8080/scan`
+  //       );
+  //       console.log(response.data)
+  //       setScanSays(response.data)
+    
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   scanResults();
+  // }, [openModal]);
 
 function revealPassword() {
   setIsShown((isShown) => !isShown);
 
 } 
+
   return (
     <div className="overlay">
       <div className="modal">
@@ -22,11 +43,9 @@ function revealPassword() {
           <div className="modal__dog-wrapper">
           <img className="modal__gif" src={sniffing} />
           <img src={chat} alt="speech bubble" className="modal__chatBubble" />
+          {/* <span className="pet__bubble-text">{scanSays.say}</span> */}
           </div>
           <div className="modal__buttons">
-            {/* <button onClick={onClose} className="modal__buttons-cancel">
-              Cancel
-            </button> */}
             <form className="modal__form" onSubmit={(e) => e.preventDefault()}>
               <input type={isShown ? "text" : "password"} placeholder="Let me scan the room.." className="modal__input-text"></input>
               <input id="checkbox" type="checkbox" checked={isShown} onChange={revealPassword} />
--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -1,12 +1,63 @@
 import "./CountDown.scss";
 import staticCount from "../../assets/images/staticCountDown.png";
 import gifCount from "../../assets/images/visualCountDown.gif";
+import { useEffect, useState } from "react";
 function CountDown() {
+    const [countDown, setCountDown] = useState(false);
+
+    useEffect(() => {
+        if(countDown) {
+            setTimeout(() => {
+                setCountDown(false);
+            }, 2000)
+        }
+    }, [countDown])
+
     return (<>
-    <img src={staticCount} alt="static countdown" className="countdown__static" />
-    <img src={gifCount} alt="gif of countdown" className="countdown__gif" />
+    <div className="countdown">
+        <div onClick={() => setCountDown(!countDown)} className="countdown__clock">
+            <img src={countDown ? gifCount : staticCount}
+            alt={countDown ? "gif of countdown" : "static countdown"} 
+            className={countDown ? "countdown__gif" : "countdown__static"}
+    />
+    </div>
+    </div>
     </>)
     
 }
 
 export default CountDown;
+
+    //  <div
+    //       onClick={() => setFlipHorse(!flipHorse)}
+    //       className="animalnoises__card horse"
+    //     >
+    //       <div
+    //         className={`animalnoises__front ${
+    //           flipHorse ? "animalnoises__horse-front" : ""
+    //         }`}
+    //       >
+    //         <audio controls>
+    //           <source
+    //             className="animalnoises__audio"
+    //             src={horse}
+    //             type="audio/mp3"
+    //           />
+    //           <p>Your browser does not support this audio</p>
+    //         </audio>
+    //       </div>
+    //       <div onClick={() => setFlipHorse(!flipHorse)} >
+    //         <div
+    //           className={`animalnoises__back ${
+    //             flipHorse ? "" : "animalnoises__horse-back"
+    //           }`}
+    //         >
+    //           <h3 className="animalnoises__text">Horse</h3>
+    //           <img
+    //             className="animalnoises__horse-gif"
+    //             src="https://bestanimations.com/media/horse-art/570970473animated-horse-gif-105.gif#.Y-JymKMBO5k.link"
+    //             alt="horse gif"
+    //           />
+    //         </div>
+    //       </div>
+    //     </div>
