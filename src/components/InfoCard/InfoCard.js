37dba0e053278312690006ea426d651b7e696ca9
Jorfishdan <jordan_fish@ymail.com>
2023-02-11 19:26:21 -0800

    redid info page styling

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import "./InfoCard.scss";
-import backArrow from "../../assets/images/backArrow.png";
+import backArrow from "../../assets/images/left-arrow.png";
+import dog4 from "../../assets/images/bark.gif";
+
 
 function InfoCard() {
 
@@ -10,21 +12,22 @@ function InfoCard() {
       <Link to="/"><img src={backArrow} alt="go back to main menu" className="infocard__back-button"></img></Link>
       <h1 className="infocard__title">Game Instructions</h1>
     <div className="infocard__wrapper">
-      <Link to="brainTeaser"><div className="infocard__card--1">Brain Teaser 
+      <Link to="brainTeaser"><div className="infocard__card--1">Head Scratchers
       <p className="infocard__card--1-info">
         This brain teaser will redirect high energy into a game of learning, communication and critical thinking.
         </p>
         </div></Link>
-        <Link to="cleanup"> <div className="infocard__card--2">Clean Up
+        <Link to="cleanup"> <div className="infocard__card--2">Lightning Clean
      <p className="infocard__card--2-info">
         This clean up game will have kids racing to clean up and eye where the proper places are to put items away.
         </p></div></Link>
-      <Link to="callbacks"><div className="infocard__card--3">Animal Calls
+      <Link to="callbacks"><div className="infocard__card--3">Sneaky Animals
       <p className="infocard__card--3-info">
         This callback game will have students quieten to listen carefully to the different animal noises to mimick them back and to identify the animals.
         </p>
       </div></Link>
       </div>
+      <img src={dog4} alt="barking dog cartoon gif" className="infocard__dog" />
     </div>
     </section>
   );
