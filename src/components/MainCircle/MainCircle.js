f877955c589fb10358960133540195f9761f6714
Jorfishdan <jordan_fish@ymail.com>
2023-02-12 11:38:53 -0800

    added mojo text to homepage

--- src/components/MainCircle/MainCircle.js
+++ src/components/MainCircle/MainCircle.js
@@ -2,7 +2,7 @@ import "./MainCircle.scss";
 import HomepageSlogan from "../HomepageSlogan/HomepageSlogan";
 import dogPic from "../../assets/images/boxer2.png";
 import dogGif from "../../assets/images/boxer.gif";
-import mojo from "../../assets/images/mojo.png";
+import mojo from "../../assets/images/lowerMojo.png";
 import { useState } from "react";
 import HomePageCards from "../HomePageCards/HomePageCards";
 function MainCircle() {
@@ -19,7 +19,7 @@ function MainCircle() {
         {/* // onMouseEnter={() => setIsAnimated(true)}
         // onMouseLeave={() => setIsAnimated(false)}> */}
             <img className="maincircle__gif-dog" alt="turquoise and white dog logo" src={dogGif} />
-            {/* <img className="maincircle__mojo" alt="mojo the virtual do says hi" src={mojo} /> */}
+            <img className="maincircle__mojo" alt="mojo the virtual do says hi" src={mojo} />
             {/* <p className="maincircle__dog-intro">Hi there! I'm your class pet Mojo. </p> */}
             {/* <img className={`maincircle__static-dog ${isAnimated ? "" : "maincircle__static-dog--visible"}`} alt="turquoise and white dog logo" src={dogPic} /> */}
             {/* <img className={`maincircle__gif-dog ${isAnimated ? "maincircle__gif-dog--visible" : ""}`} src={dogGif} alt="turquoise and white dog logo that's animated"/> */}
