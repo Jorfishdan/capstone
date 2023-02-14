94e7608f978c87c051a2f1d2427b9f6c0e1a6ad1
Jorfishdan <jordan_fish@ymail.com>
2023-02-13 18:28:37 -0800

    cleaned up CallBacks component

--- src/pages/CallBacks/CallBacks.js
+++ src/pages/CallBacks/CallBacks.js
@@ -1,45 +1,15 @@
 import "./CallBacks.scss"
-// import AnimalNoises from "../../components/AnimalNoises/AnimalNoises";
 import AnimalNoises from "../../components/AnimalNoises/AnimalNoises";
 import CallBackPts from "../../components/CallBackPts/CallBackPts";
-import house from "../../assets/images/house-static.png";
-import { Link } from "react-router-dom";
-import vetPic from "../../assets/images/veterinary.png";
-import dogFood from "../../assets/images/dog-food.png";
 import CountDown from "../../components/CountDown/CountDown";
 import CallBackPets from "../../components/CallbackPet/CallbackPet";
 import HPBubbles from "../../components/HPBubbles/HPBubbles";
+import CallBacksHeader from "../../components/CallBacksHeader/CallBacksHeader";
 
 function Callbacks() {
     return (
         <>
-        <div className="callbacks">
-        <Link to="/">
-        <img
-          src={house}
-          alt="home icon to return to main menu"
-          className="callbacks__home"
-        />
-      </Link>
-      <span className="callbacks__title">Teacher's Pet</span>
-      <div className="callbacks__icon-wrapper">
-      <Link to="/brainteaser">
-        <img
-          src={vetPic}
-          alt="icon to brainteaser page"
-          className="callbacks__brainteaser-link"
-        />
-      </Link>
-      <Link to="/cleanup">
-        <img
-          src={dogFood}
-          alt="icon to cleanup page"
-          className="callbacks__cleanup-link"
-        />
-      </Link>
-      </div>
-      </div>
-      
+        <CallBacksHeader /> 
         <CountDown />
         <CallBackPts />
         <CallBackPets />
