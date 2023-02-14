7d31b1d6b1e2fbba0ae31cde54e28d55969892ef
Jorfishdan <jordan_fish@ymail.com>
2023-02-14 09:34:45 -0800

    edited infocard to shorten instructions

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -21,8 +21,7 @@ function InfoCard() {
             <div className="infocard__card--1">
               Head Scratchers
               <p className="infocard__card--1-info">
-                This true or false game will redirect high energy into a game of
-                learning, communication and critical thinking. Click the card of choice and the response will show with the corresponding points. Add to the deck with clicking on the "Add fact here" button on the right side of screen
+               Click the card of choice and the response will show with the corresponding points. Add to the deck with clicking on the "Add fact here" button on the right side of screen
               </p>
             </div>
           </Link>
@@ -31,8 +30,7 @@ function InfoCard() {
             <div className="infocard__card--2">
               Lightning Clean
               <p className="infocard__card--2-info">
-                This timed game will have kids racing to clean up and eye
-                where the proper places are to put items away while dancing along to some hiphop. Afterwards, click 'scan' and type out points with feedback that'll be from Mojo. This response will be hidden until you as the educator clicks the checkbox next to the input bar
+               Play a song and start the clock to speed clean. Afterwards, click 'scan' and type out points with feedback that'll be from Mojo. This response will be hidden until you as the educator clicks the checkbox next to the input bar
               </p>
             </div>
           </Link>
@@ -40,7 +38,7 @@ function InfoCard() {
             <div className="infocard__card--3">
               Sneaky Animals
               <p className="infocard__card--3-info">
-                Your students will shush each other to listen carefully in order to correctly match the noise to the animals! After each card, you can either turn the sound down on your device or directly on the card to make the game increasingly challenging
+                Have students guys the animal based on its sound. After each card, you can either turn the sound down on your device or directly on the card to make the game increasingly challenging
               </p>
             </div>
           </Link>
