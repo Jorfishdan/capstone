e06cf15dd7835f9068979548e0ae6c37f7180f5c
Jorfishdan <jordan_fish@ymail.com>
2023-02-13 16:06:50 -0800

    cleaned homepagecards component and created homepagecardcircle componenent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,17 @@
 import "./Header.scss";
-import info from "../../assets/images/info.png";
-import information from "../../assets/images/information.gif"
 import staticInfo from "../../assets/images/info-static.png";
 import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        
-        <section className="header">
-            <div className="header__wrapper">
-                {/* <img src={logout} alt="logout icon" className="header__logout" /> */}
-                {/* <img src={list} alt="list icon to edit profile" className="header__list"></img> */}
-               <Link to="/mainInfo"><img src={staticInfo}className="header__info"></img></Link>                
-            </div>
-        </section>
-    )
+  return (
+    <section className="header">
+      <div className="header__wrapper">
+        <Link to="/mainInfo">
+          <img src={staticInfo} className="header__info"></img>
+        </Link>
+      </div>
+    </section>
+  );
 }
 
 export default Header;
--- src/components/HomePageCards/HomePageCards.js
+++ src/components/HomePageCards/HomePageCards.js
@@ -1,30 +1,26 @@
 import "./HomePageCards.scss";
-import dog from "../../assets/images/dogMascot.gif"
+import HomepageCardCircle from "../HomepageCardCircle/HomepageCardCircle";
 import { Link } from "react-router-dom";
 
 function HomePageCards() {
   return (
     <>
-    <section className="homepagecard">
-    
-      <div className="homepagecard__mega-wrapper">
-      <div className="homepagecard__1"></div>
-            <div className="homepagecard__2"></div>
-            <div className="homepagecard__3"></div>
-            <div className="homepagecard__4"></div>
-            <div className="homepagecard__5"></div>
-            <div className="homepagecard__6"></div>
-      {/* <h1 className="homepagecard__title">Teacher's Pet</h1>
-      <img className="homepagecard__dog" src={dog} alt="dog" /> */}
-     
-    <article className="homepagecard__wrapper">
-     <Link to="/brainTeaser"><h1 className="homepagecard__card--1">Head Scatchers</h1></Link> 
-      <Link to="/cleanup"><h1 className="homepagecard__card--2">Lightning Clean</h1></Link>
-      <Link to="/callbacks"><h1 className="homepagecard__card--3">Sneaky Animals</h1></Link>
-    </article>
-    </div>
-  
-    </section>
+      <section className="homepagecard">
+        <div className="homepagecard__mega-wrapper">
+          <HomepageCardCircle />
+          <article className="homepagecard__wrapper">
+            <Link to="/brainTeaser">
+              <h1 className="homepagecard__card--1">Head Scatchers</h1>
+            </Link>
+            <Link to="/cleanup">
+              <h1 className="homepagecard__card--2">Lightning Clean</h1>
+            </Link>
+            <Link to="/callbacks">
+              <h1 className="homepagecard__card--3">Sneaky Animals</h1>
+            </Link>
+          </article>
+        </div>
+      </section>
     </>
   );
 }
