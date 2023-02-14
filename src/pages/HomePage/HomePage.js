aba9423cc5454dec57dae2b44c9779068fd51835
Jorfishdan <jordan_fish@ymail.com>
2023-02-13 19:06:23 -0800

    clean up homepage code

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,37 +1,20 @@
 import "./HomePage.scss";
-import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import HomePageCards from "../../components/HomePageCards/HomePageCards";
-import dog from "../../assets/images/dogMascot.gif";
-import dog2 from "../../assets/images/dog2.png";
-import HomepageSlogan from "../../components/HomepageSlogan/HomepageSlogan";
 import HPBubbles from "../../components/HPBubbles/HPBubbles";
 import MainCircle from "../../components/MainCircle/MainCircle";
-import info from "../../assets/images/info-static.png";
-import { Link } from "react-router-dom";
-
+import HomePageBubbles from "../../components/HomePageBubbles/HomePageBubbles";
+import HomePageHeader from "../../components/HomePageHeader/HomePageHeader";
 
 function HomePage() {
   return (
     <>
-      <section className="homepage">
-        <div className="homepage__info-wrapper">
-      <Link to="/mainInfo"><img src={info} alt="info icon to read game instructions" className="homepage__info" /></Link>
-      </div>
+     
+        <HomePageHeader />
         <MainCircle />
-        <div className="homepage__main-wrapper">
-         
-        </div>
-        <div className="homepage__circle3"></div>
-        <div className="homepage__circle4"></div>
-        <div className="homepage__circle5"></div>
-        <div className="homepage__circle6"></div>
-        <div className="homepage__circle7"></div>
-        <div className="homepage__circle8"></div>
-        <div className="homepage__circle9"></div>
+        <HomePageBubbles />
         <HPBubbles />
         <Footer />
-      </section>
+      
     </>
   );
 }
