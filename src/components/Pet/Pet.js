4c34640a0f3b948d06aedd0f7d4f37abdebdb9fd
Jorfishdan <jordan_fish@ymail.com>
2023-02-13 17:32:42 -0800

    cleaned up components Pets, SubmitFact and Timer

--- src/components/Pet/Pet.js
+++ src/components/Pet/Pet.js
@@ -1,75 +1,63 @@
-import "./Pet.scss"
+import "./Pet.scss";
 import cheering from "../../assets/images/cheering.gif";
 import chat from "../../assets/images/chat.png";
 import { useEffect, useState } from "react";
 import axios from "axios";
 function Pet() {
-    const [isPetShown, setIsPetShown] = useState(false);
-    const [dogBubble, setDogBubble] = useState([]);
-    const [sayIndex, setSayIndex] = useState(0);
-    const [isRolledOut, SetIsRolledOut] = useState(false);
+  const [isPetShown, setIsPetShown] = useState(false);
+  const [dogBubble, setDogBubble] = useState([]);
+  const [sayIndex, setSayIndex] = useState(0);
+  const [isRolledOut, SetIsRolledOut] = useState(false);
 
-    // function rollOut() {
-    //   SetIsRolledOut(true)
-    // }
+  useEffect(() => {
+    setTimeout(() => {
+      setIsPetShown(true);
+      SetIsRolledOut(true);
+    }, 3000);
+  }, [isPetShown]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsPetShown(true)
-            SetIsRolledOut(true)
-            
-        }, 3000)
-    }, [isPetShown])
-
-    useEffect(() => {
-        if(isPetShown) {
-        setTimeout(() => {
-          setIsPetShown(false)
-           
-        }, 10000)
+  useEffect(() => {
+    if (isPetShown) {
+      setTimeout(() => {
+        setIsPetShown(false);
+      }, 10000);
     }
-    }, [isPetShown])
+  }, [isPetShown]);
 
-    useEffect(() => {
-        const dogSays = async () => {
-          try {
-            const response = await axios.get(
-              ` http://localhost:8080/dog`
-            );
-            console.log(response.data)
-            setDogBubble(response.data)
-        
-          } catch (error) {
-            console.log(error);
-          }
-        };
-        dogSays();
-      }, []);
+  useEffect(() => {
+    const dogSays = async () => {
+      try {
+        const response = await axios.get(` http://localhost:8080/dog`);
+        console.log(response.data);
+        setDogBubble(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    dogSays();
+  }, []);
 
-      useEffect(() => {
-        if(isPetShown) {
-            setTimeout(() => {
-                setSayIndex(sayIndex === dogBubble.length -1 ? 0 : sayIndex +1)
-            }, 10000)
-        }
-      }, [isPetShown, sayIndex, sayIndex.length])
+  useEffect(() => {
+    if (isPetShown) {
+      setTimeout(() => {
+        setSayIndex(sayIndex === dogBubble.length - 1 ? 0 : sayIndex + 1);
+      }, 10000);
+    }
+  }, [isPetShown, sayIndex, sayIndex.length]);
 
-
-
-    return (
-        <div className="pet">
-            {isPetShown && ( 
-            <div className="pet__wrapper">
-            <img src={cheering} alt="speech bubble" className="pet__cheering" />
-            <div className="pet__bubble-wrap">
+  return (
+    <div className="pet">
+      {isPetShown && (
+        <div className="pet__wrapper">
+          <img src={cheering} alt="speech bubble" className="pet__cheering" />
+          <div className="pet__bubble-wrap">
             <img src={chat} alt="speech bubble" className="pet__chat" />
             <span className="pet__bubble-text">{dogBubble[sayIndex].say}</span>
-            </div>
-            
+          </div>
         </div>
-        )}
-        </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default Pet;
--- src/components/SubmitFact/SubmitFact.js
+++ src/components/SubmitFact/SubmitFact.js
@@ -1,63 +1,76 @@
-import axios from 'axios';
-import { useRef, useState } from 'react';
-import {v4 as uuidv4} from "uuid";
-import './SubmitFact.scss';
+import axios from "axios";
+import { useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./SubmitFact.scss";
 
+function SumbitFact() {
+  const formRef = useRef(null);
 
-function SumbitFact() { 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = formRef.current;
+    const question = form.question.value;
+    const answer = form.answer.value;
+    const incorrect = form.incorrect.value;
+    const explain = form.explain.value;
 
-    const formRef = useRef(null);
-    // const [isShown, setIsShown] = useState(false);
-
-    function handleSubmit(event) {
-        event.preventDefault()
-        const form = formRef.current;
-        const question = form.question.value;
-        const answer = form.answer.value;
-        const incorrect = form.incorrect.value;
-        const explain = form.explain.value;
-
-        if ( !question || !answer || !incorrect || !explain) {
-            alert("Please fill out all the fields")
-            return;
-        }
+    if (!question || !answer || !incorrect || !explain) {
+      alert("Please fill out all the fields");
+      return;
+    }
 
     axios.post(`http://localhost:8080/facts`, {
-        id: uuidv4(),
-        question: question,
-        correct_answer: answer,
-        incorrect_answer: incorrect,
-        explanation: explain,
-    })
-    alert("It's been added to the game!")  
-    // formRef.current.reset();
+      id: uuidv4(),
+      question: question,
+      correct_answer: answer,
+      incorrect_answer: incorrect,
+      explanation: explain,
+    });
+    alert("It's been added to the game!");
+  }
 
-};
+  return (
+    <>
+      
+      <form onSubmit={handleSubmit} ref={formRef} className="form__submit">
+        <h2 className="form__add-question">Add a question to the game!</h2>
+        <input
+          type="text"
+          className="form__input"
+          name="question"
+          placeholder="Add a question..."
+        ></input>
 
-    return (
-        <>
-        {/* {isShown && ( */}
-        {/* <div className="form__wrapper"> */}
-        <form onSubmit={handleSubmit} ref={formRef} className="form__submit">
-            <h2 className="form__add-question">Add a question to the game!</h2>
-            <input type="text" className="form__input" name="question" placeholder="Add a question..."></input>
+        <h2 className="form__add-answer">What is the correct answer?</h2>
+        <input
+          className="form__answer"
+          type="text"
+          name="answer"
+          placeholder="Is it true or false?"
+        ></input>
 
-            <h2 className="form__add-answer">What is the correct answer?</h2>
-            <input className="form__answer" type="text" name="answer" placeholder="Is it true or false?"></input>
+        <h2 className="form__add-incorrect">What is the incorrect answer?</h2>
+        <input
+          className="form__incorrect"
+          type="text"
+          name="incorrect"
+          placeholder="Enter it here..."
+        ></input>
 
-            <h2 className="form__add-incorrect">What is the incorrect answer?</h2>
-            <input className="form__incorrect" type="text" name="incorrect" placeholder="Enter it here..."></input>
+        <h2 className="form__add-explain">Add an explanantion</h2>
+        <input
+          className="form__explain"
+          type="text"
+          name="explain"
+          placeholder="The more you know :)"
+        ></input>
 
-            <h2 className="form__add-explain">Add an explanantion</h2>
-            <input className="form__explain" type="text" name="explain" placeholder="The more you know :)"></input>
-
-            <button className="form__button" onClick={handleSubmit} >Submit</button>
-
-        </form>
-        {/* </div> */}
-        {/* )} */}
-        </>
-    )
+        <button className="form__button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default SumbitFact;
