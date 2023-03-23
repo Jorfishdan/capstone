5fee6b08ba5918e02911eb2b43a8465c8d7b1ef2
Jorfishdan <jordan_fish@ymail.com>
2023-03-23 14:29:22 -0700

    board and cards set up

--- src/components/DataCard/DataCard.js
+++ src/components/DataCard/DataCard.js
@@ -1,5 +1,31 @@
 import './DataCard.scss';
-function DataCard() {
+function DataCard(props) {
+     
+  let Cardclasses = ["Card"];
+  let contentClasses  = ["iconify"];
+  
+  if(props.active){
+    Cardclasses.push("active");
+  }
+  
+  return (
+    <button 
+      className={Cardclasses.join(" ")}
+      onClick={props.clicked}
+      disabled={props.active} 
+    >
+      <span 
+        className={contentClasses.join(" ")} 
+        data-icon={props.value} 
+        data-inline="false" 
+        data-width="60" 
+        data-height="60"
+      >
+      </span>
+    </button>
+  );
+    
+
     
 }
 export default DataCard;
--- src/components/MatchingGame/MatchingGame.js
+++ src/components/MatchingGame/MatchingGame.js
@@ -1,63 +1,107 @@
 import "./MatchingGame.scss";
 import { useState, useEffect } from "react";
+import DataCard from "../DataCard/DataCard";
 
 function MatchingGame() {
-  const [cards, setCards] = useState([
-    { id: 1, value: 13, type: 'number', flipped: false },
-    { id: 2, value: 6, type: 'number', flipped: false },
-    { id: 3, value: 9, type: 'number', flipped: false },
-    { id: 4, value: 12, type: 'number', flipped: false },
-    { id: 5, value: 15, type: 'number', flipped: false },
-    { id: 6, value: 18, type: 'number', flipped: false },
-    { id: 7, value: 3, type: 'number', flipped: false },
-    { id: 8, value: 14, type: 'number', flipped: false },
-    { id: 9, value: 11, type: 'number', flipped: false },
-    { id: 10, equation: '6+7=', type: 'equation', flipped: false },
-    { id: 11, equation: '10-4=', type: 'equation', flipped: false },
-    { id: 12, equation: '6+3=', type: 'equation', flipped: false },
-    { id: 13, equation: '10+2=', type: 'equation', flipped: false },
-    { id: 14, equation: '18-3=', type: 'equation', flipped: false },
-    { id: 15, equation: '9+9=', type: 'equation', flipped: false },
-    { id: 16, equation: '5-2=', type: 'equation', flipped: false },
-    { id: 17, equation: '8+6=', type: 'equation', flipped: false },
-    { id: 18, equation: '20-9=', type: 'equation', flipped: false },
-  ]);
 
- 
-
-  function shuffleArray(array) {
-for (let i = array.length -1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i +1));
-  [array[i], array[j]] = [array[j], array[i]];
+function randomSort(a,b) {
+  return 0.5 - Math.random();
 }
-return array
+
+const val = ["noto:bird", "noto:blossom", "noto:cactus", "noto:avocado", "noto:cookie", "noto:crystal-ball", "noto:peach", "noto:gorilla"];
+const values = [...val.concat(val)].sort(randomSort);
+
+const [cards, setCards] = useState([...Array(16).keys()].map(n => false));
+const [selected, setSelected] = useState([]);
+const [hits, setHits] = useState(0);
+
+const clickHandler = (key) => {
+  let newCards = [...cards];
+  let newSelected = [...selected];
+  let newHits = hits;
+  if(newSelected.length > 1) {
+    newHits++;
+    newCards[newSelected[0]] = false;
+    newCards[newSelected[1]] = false;
+    newSelected = [];
   }
-  useEffect(() => {
-    setCards(shuffleArray(cards))
-  }, [])
+  newCards[key] = true;
+  newSelected.push(key);
+  if(newSelected.length > 1 && values[newSelected[0]] === values[newSelected[1]]) {
+    newSelected = [];
+  }
+  setCards(newCards);
+  setSelected(newSelected);
+  setHits(newHits);
+}
 
-const handleClick = (id) => {
-  const newCards = [...cards];
-  const cardIndex = newCards.findIndex((card) => card.id === id);
-  newCards[cardIndex].flipped = true;
-  setCards(newCards)
-};
+const cardElements = cards.map((_, cardIndex) => (
+  <DataCard
+    key={cardIndex} 
+    value={values[cardIndex]}
+    active={cards[cardIndex]}
+    clicked={() => clickHandler(cardIndex)}
+  />
+));
 
-  return (
-    <div className="card-container">
-      {cards.map((card)=> (
-        <div 
-        key={card.id}
-        className={`card ${card.flipped ? "flipped" : ""}`}
-        onClick={()=> handleClick(card.id)}
-        >
-          <div className="front">{card.value || card.equation}</div>
-          <div className="back"></div>
-          </div>
-      ))}
-</div>
-  
-  );
+return (
+  <>
+    <h1>Memory Game</h1>
+    <div className="Board">{cardElements}</div>
+    <p>Hits: {hits}</p>
+  </>
+);
 }
 
 export default MatchingGame;
+
+
+  /* <div
+onClick={() => setFlipHorse(!flipHorse)}
+className="animalnoises__card horse"
+>
+<div
+  className={`animalnoises__front ${
+    flipHorse ? "animalnoises__animal-front" : ""
+  }`}
+>
+  <audio controls>
+    <source
+      className="animalnoises__audio"
+      src={horse}
+      type="audio/mp3"
+    />
+    <p>Your browser does not support this audio</p>
+  </audio>
+</div>
+<div onClick={() => setFlipHorse(!flipHorse)}>
+  <div
+    className={`animalnoises__back ${
+      flipHorse ? "" : "animalnoises__animal-back"
+    }`}
+  >
+    <h3 className="animalnoises__text-horse">Horse</h3>
+    <img
+      className="animalnoises__horse-gif"
+      src="https://bestanimations.com/media/horse-art/570970473animated-horse-gif-105.gif#.Y-JymKMBO5k.link"
+      alt="horse gif"
+    />
+  </div>
+</div>
+</div> */
+
+
+// const [selectedCards, setSelectedCards] = useState([]);
+// const [matchedCards, setMatchedCards] = useState([]);
+
+//   function shuffleArray(array) {
+// for (let i = array.length -1; i > 0; i--) {
+//   const j = Math.floor(Math.random() * (i +1));
+//   [array[i], array[j]] = [array[j], array[i]];
+// }
+// return array
+//   }
+
+//   useEffect(() => {
+//     setCards(shuffleArray(cards))
+//   }, [])
